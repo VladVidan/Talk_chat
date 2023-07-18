6597f5a2534fe156e8b3b2b8edfe5c49a746415b
Oleksandr <Brusiltsev1@gmail.com>
2023-07-18 11:54:33 +0300

    авторизация  правки /аутификация через simplejwt и авторизация/обновление /просмотра профиля через djoser

--- talk_app/talk_app/urls.py
+++ talk_app/talk_app/urls.py
@@ -18,11 +18,18 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('api/v1/talk-auth/', include('rest_framework.urls')),
+#     path('api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+#     path('api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+#     path( 'api/v1/auth/', include('djoser.urls')),
+#     re_path(r'^auth/', include('djoser.urls.authtoken')), #
+# ]
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/talk-auth/', include('rest_framework.urls')),
-    path('api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/v1/talk-auth/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path( 'api/v1/auth/', include('djoser.urls')),
-    re_path(r'^auth/', include('djoser.urls.authtoken')), #
+    path('api/v1/auth/', include('djoser.urls')),
+    path('api/v1/auth/users/me/', include('djoser.urls')),
 ]
