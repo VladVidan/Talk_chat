1526a3d30a5eb7094ebf2b21d6e42bb9d47c7b1a
Vlad_Vidan <kozlovvlad1994@gmail.com>
2023-07-31 20:33:25 +0300

    PackagesUpdate

--- talk_app/talk_core/views.py
+++ talk_app/talk_core/views.py
@@ -1,4 +1,7 @@
+from typing import Generic
+
 from django.contrib.auth import get_user_model
+from django_rest_passwordreset.models import ResetPasswordToken
 from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -6,12 +9,15 @@ from rest_framework import status
 from django.core.mail import send_mail
 from django.contrib.auth.models import User
 from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.views import TokenViewBase, TokenRefreshView
+
 from .serializers import EmailLoginSerializer
 from django.shortcuts import redirect
 
 
 class UserRegistrationAPIView(APIView):
     """Registration class"""
+
     def post(self, request):
         """"Processing registration form data and creating a new user"""
 
@@ -51,6 +57,7 @@ def confirm_account(request, user_id):
 
 class EmailLoginAPIView(APIView):
     """Custom class for login with email"""
+
     def post(self, request):
         serializer = EmailLoginSerializer(data=request.data)
         if serializer.is_valid():
@@ -70,3 +77,50 @@ class EmailLoginAPIView(APIView):
             else:
                 return Response({'error': 'Неверные учетные данные'}, status=status.HTTP_401_UNAUTHORIZED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class PasswordResetAPIView(APIView):
+    def post(self, request):
+        email = request.data.get('email')
+
+        try:
+            user = get_user_model().objects.get(email=email)
+        except get_user_model().DoesNotExist:
+            return Response({"message": "User with this email does not exist."}, status=status.HTTP_404_NOT_FOUND)
+
+        reset_token = ResetPasswordToken.objects.create(user=user)
+
+        reset_url = 'https://valerka4052.github.io/chat-talk-front/recover-password/?token=' + reset_token.key
+        message = f'To reset your password, follow this link: {reset_url}'
+        from_email = 'talk.team.challenge@gmail.com'
+        to_email = email
+        send_mail('Password Reset', message, from_email, [to_email])
+
+        return Response({"message": "Password reset email has been sent. Check your email to reset your password."},
+                        status=status.HTTP_200_OK)
+
+
+class PasswordResetConfirmAPIView(APIView):
+    def post(self, request):
+        token = request.data.get('token')
+        new_password = request.data.get('new_password')
+
+        try:
+            reset_token = ResetPasswordToken.objects.get(key=token)
+
+
+        except ResetPasswordToken.DoesNotExist:
+            return Response({"message": "Invalid or expired token."}, status=status.HTTP_400_BAD_REQUEST)
+
+        if reset_token.is_expired():
+            return Response({"message": "Invalid or expired token."}, status=status.HTTP_400_BAD_REQUEST)
+
+        user = reset_token.user
+        user.set_password(new_password)
+        user.save()
+
+        reset_token.delete()
+
+        return Response({"message": "Password has been reset successfully."}, status=status.HTTP_200_OK)
+
+
