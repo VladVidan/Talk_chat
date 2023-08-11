6dbf62502be9bedc83fad26aac10c4be69c58f83
Alex <124603897+OleksandrBrusyltsev@users.noreply.github.com>
2023-08-11 21:24:34 +0300

    add username for registration

--- talk_app/talk_core/serializers.py
+++ talk_app/talk_core/serializers.py
@@ -1,10 +1,22 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
 
+
 class EmailLoginSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(write_only=True, required=True)
+    password = serializers.CharField(write_only=True, required=True, style={'input_type': 'password'})
+
+
+    class Meta:
+        model = User
+        fields = ['email', 'password']
+
+
+class UsernameRegisterSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(write_only=True, required=True)
     email = serializers.EmailField(write_only=True, required=True)
     password = serializers.CharField(write_only=True, required=True, style={'input_type': 'password'})
 
     class Meta:
         model = User
-        fields = ['email', 'password']
+        fields = ['username' , 'email', 'password']
